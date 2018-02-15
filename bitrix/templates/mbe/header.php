0db6f3c9c7d2d5d113b45a79a76d76d55ab09532
adoletskiy <adoletsky@indigos.ru>
2018-02-15 18:05:59 +0300

    debug og:url & description

--- bitrix/templates/mbe/header.php
+++ bitrix/templates/mbe/header.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title><?$APPLICATION->ShowTitle()?></title>
-    <meta name="description" content="MBE мировой — лидер в области экспресс-доставки"/>
+    <?/*?><meta name="description" content="MBE мировой — лидер в области экспресс-доставки"/><?*/?>
     <meta name="robots" content="index, follow"/>
     <!--//Meta-->
 
